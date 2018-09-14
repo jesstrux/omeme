11f2b01720179f8f95ff16a5cf9fbb51f616bd21
Walter Kimaro <wakyj07@gmail.com>
2018-09-14 19:37:06 +0300

    Added search interaction

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -8,17 +8,22 @@ import Tabs from "./Tabs";
 const Header = ( props ) => {
     const { loading } = props;
 
+    function handleClearSearch(){
+      document.querySelector("#searchWrapper input").value = "";
+      props.onSearch("");
+    }
+
     return ( 
         <header className={'App-header' + (loading ? ' loading' : '')}>
           <div id="toolbar">
             <img src={logo} className="App-logo" alt="logo" />
               <span className="App-title">Omeme</span>
               <div id="searchWrapper">
-                <input type="text" placeholder="Search for gifs & memes..."
-                  onKeyUp={ e => props.onSearch(e.target.value) }/>
+                <input type="text" required placeholder="Search for gifs & memes..."
+                  onKeyUp={ e => props.onSearch(e.target.value) } />
                 
-                <button onClick={ props.clearSearch }>
-                  <svg width="24" height="24" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
+                <button onClick={ handleClearSearch }>
+                  <svg width="12" height="12" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
                 </button>
               </div>
             <button>CREATE</button>
